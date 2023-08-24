dffff60af1dfe0b4905c4ad6b519f7cb2adcb55b
Thiago Ferreira Rodrigues <thiago.ferreirarod22@gmail.com>
2023-08-23 21:44:56 -0300

    feat: start product module development

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Route, Router } from '@angular/router';
+import { ProductModel } from '@app/@shared/models/products.model';
+import { ProductService } from '@app/@shared/services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -6,7 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly route: ActivatedRoute, private readonly productService: ProductService) {}
 
-  ngOnInit(): void {}
+  public busy$: Subscription[] = [];
+  public products: ProductModel[] = [];
+  public displayedColumns: string[] = ['image', 'value', 'acquisitionPrice', 'sellingPrice', 'quantity', 'action'];
+
+  ngOnInit(): void {
+    this.productsData();
+  }
+
+  ngOnDestroy() {
+    this.busy$.forEach((subscription) => subscription.unsubscribe());
+  }
+
+  private productsData() {
+    this.busy$.push(
+      this.productService.getProducts().subscribe({
+        next: (data) => {
+          this.products = data;
+        },
+        error: (err) => {},
+      })
+    );
+  }
 }
